8132071460c057004c94b73287ee5926849b7a53
agustin <agustin.pacheco123@gmail.com>
2026-08-25 23:01:36 -0400

    validador horizontal (listo)

--- validacion.py
+++ validacion.py
@@ -4,11 +4,22 @@ class validador:
 
     def verificar_fila(self, tablero,fila:int,turno:str) :
         contador= 0
-        for col in tablero.grilla[fila]:
-            if col == turno:
+        for col in range(tablero.dimension):
+            if tablero.grilla[fila][col] == turno:
                 contador +=1
-            if contador == 5:
-                return True
+                if contador == 5:
+                    return True
             else:
                 contador = 0
         return False
+    def verificar_columna(self,tablero,columna:int,turno:str):
+        contador= 0
+        for fil in range(tablero.dimension):
+            if tablero.grilla[fil][columna] == turno:                        
+                contador +=1
+                if contador == 5:
+                    return True
+            else:
+                contador = 0
+        return False
+
