2214d667e2e86bb1253abfeb6cd8ad2e852867b1
agustin <agustin.pacheco123@gmail.com>
2026-08-25 23:53:23 -0400

    validadores diagonales(listos)

--- validacion.py
+++ validacion.py
@@ -12,6 +12,7 @@ class validador:
             else:
                 contador = 0
         return False
+    
     def verificar_columna(self,tablero,columna:int,turno:str):
         contador= 0
         for fil in range(tablero.dimension):
@@ -22,4 +23,39 @@ class validador:
             else:
                 contador = 0
         return False
-
+    
+    def verificar_diagonal_negativa(self,tablero,fila:int,columna:int,turno: str):
+        contador = 1
+        for i in range(1,5):
+            if tablero.esta_en_rango(fila + i,columna + i) and tablero.grilla[fila + i][(columna + i)] == turno:
+                contador += 1
+                if contador == 5:
+                    return True
+            else:
+                break
+        for j in range(1,5):
+            if tablero.esta_en_rango(fila - j,columna - j) and tablero.grilla[fila - j][(columna - j)] == turno:
+                contador +=1
+                if contador == 5:
+                    return True
+            else:
+                break
+        return False
+    
+    def verificar_diagonal_positiva(self,tablero,fila:int,columna:int,turno: str):
+        contador = 1
+        for i in range(1,5):
+            if tablero.esta_en_rango(fila - i,columna + i) and tablero.grilla[fila - i][(columna + i)] == turno:
+                contador += 1
+                if contador == 5:
+                    return True
+            else:
+                break  
+        for j in range(1,5):
+            if tablero.esta_en_rango(fila + j,columna - j) and tablero.grilla[fila + j][(columna - j)] == turno:
+                contador +=1
+                if contador == 5:
+                    return True
+            else:
+                break
+        return False
