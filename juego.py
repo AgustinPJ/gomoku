81bca8d6efe11ef22d8c98860ac61b8f8077a2f9
agustin <agustin.pacheco123@gmail.com>
2026-08-29 00:04:01 -0400

    version funcional 1.0

--- juego.py
+++ juego.py
@@ -8,21 +8,20 @@ class Juego:
         self.turnos_jugados = 0
         self.estado = "jugando"
     def jugada(self,fila:int,columna:int): # evalua si la jugada es valida y el estado de la partida
-        if self.estado!= "jugando":
-            return (False,"la partida ya finalizo")
         if self.tablero.colocar_ficha(fila,columna,self.turno):
             self.turnos_jugados += 1
             if self.validador.validar_ganador(self.tablero,fila,columna,self.turno):
-                self.estado =f"ganador {self.turno}"
+                self.estado =f"----- ganador {self.turno} -----"
                 return (True,self.estado)
-            if self.validador.empate(self.tablero,self.turnos_jugados):
-                self.estado = "hay empate"
+            if self.validador.validar_empate(self.tablero,self.turnos_jugados):
+                self.estado = "----- hay empate -----"
                 return (True,self.estado)
             if self.turno == "A":
                 self.turno = "B"
             else:
                 self.turno = "A"
-            return (True,"jugada valida")
+            return (True,"----- jugada valida -----")
         
         else:
-            return (False,"jugada invalida")
+            return (False,"----- jugada invalida -----")
+        
--- main.py
+++ main.py
@@ -4,21 +4,38 @@ from tablero import Tablero
 def mostrar_tablero(tablero:Tablero):# muestra el tablero
    print( "  "+" ".join ( str(i+1)  for i in range(tablero.dimension))) #fila de indices
    for j in range(tablero.dimension):
-        print((str(j+1)+" "+" ".join("."if celda is None else str(celda) for celda in tablero.grilla[j]))) #muestra el indice de columna + fila
+        print((str(j+1)+" "+" ".join("."if celda is None else str(celda) for celda in tablero.grilla[j]))) #muestra el indice de columna + fila grilla
 def pedir_dimension():
     while True:
         try:
-            dimension = int(input("indica dimension del tablero(>=5): "))
+            dimension = int(input("indica dimension del tablero(mayor a 5 y menor a 10 ): "))
             if dimension <5:
                 print("ERROR:dimension indicada muy pequeña, ingrese una dimension valida")
                 continue
-            break
+            return dimension
                 
         except ValueError:
             print("ERROR:ingrese un numero entero por favor")
-def iniciar():
-    dimension = pedir_dimension
-    juego = juego(dimension)
-    mostrar_tablero(juego)
-    while juego.self.estado == "jugando":
-        print("-----")
+def main():
+    dimension = pedir_dimension()
+    juego = Juego(dimension)
+    print("----- juego iniciado -----")
+    while juego.estado == "jugando":
+        mostrar_tablero(juego.tablero)
+        print(f"----- Turno {juego.turno}----- ")
+        fila = int(input("ingrese indice de la fila: ")) - 1
+        columna = int(input("ingrese indice de la columna: ")) - 1
+        valida, mensaje =juego.jugada(fila,columna)
+        print({mensaje})
+    mostrar_tablero(juego.tablero)
+    try:    
+        seguir = int(input("\nvolver a jugar 1.si / 2.no : " ))
+        if seguir == 1:
+            main()
+        elif seguir ==2:
+            print("----- gracias por jugar -----")
+    except ValueError:
+            print(" ingrese opcion valida (1 o 2)")
+
+    
+main()
