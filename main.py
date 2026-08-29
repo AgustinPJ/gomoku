c6fe0733b27515474841c169ff24b461c97f3dc4
agustin <agustin.pacheco123@gmail.com>
2026-08-29 01:30:31 -0400

    version funcional 1.1

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from juego import Juego
 from tablero import Tablero
 
 def mostrar_tablero(tablero:Tablero):# muestra el tablero
-   print( "  "+" ".join ( str(i+1)  for i in range(tablero.dimension))) #fila de indices
-   for j in range(tablero.dimension):
-        print((str(j+1)+" "+" ".join("."if celda is None else str(celda) for celda in tablero.grilla[j]))) #muestra el indice de columna + fila grilla
+    print( "   "+" ".join ( f"{i+1:>2}"  for i in range(tablero.dimension))) #fila de indices
+    for j in range(tablero.dimension):
+            print((f"{j+1:>2} "+" ".join(" ."if celda is None else f"{celda:>2}" for celda in tablero.grilla[j]))) #muestra el indice de columna + fila grilla
 def pedir_dimension():
     while True:
         try:
-            dimension = int(input("indica dimension del tablero(mayor a 5 y menor a 10 ): "))
+            dimension = int(input("indica dimension del tablero(>=5): "))
             if dimension <5:
                 print("ERROR:dimension indicada muy pequeña, ingrese una dimension valida")
                 continue
