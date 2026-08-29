e29c12c3636aa2862db4f47d78e5aa9c8e306dae
agustin <agustin.pacheco123@gmail.com>
2026-08-29 01:54:07 -0400

    cumplimiento PEP 8

--- juego.py
+++ juego.py
@@ -1,27 +1,27 @@
 from tablero import Tablero
 from validacion import validador
 class Juego:
-    def __init__(self,dimension:int): # inicia el juego
+    def __init__(self, dimension: int):  # inicia el juego
         self.tablero =  Tablero(dimension)
         self.validador = validador()
         self.turno = "A"
         self.turnos_jugados = 0
         self.estado = "jugando"
-    def jugada(self,fila:int,columna:int): # evalua si la jugada es valida y el estado de la partida
-        if self.tablero.colocar_ficha(fila,columna,self.turno):
+    def jugada(self, fila: int, columna: int):  # evalua si la jugada es valida y el estado de la partida
+        if self.tablero.colocar_ficha(fila, columna, self.turno):
             self.turnos_jugados += 1
-            if self.validador.validar_ganador(self.tablero,fila,columna,self.turno):
-                self.estado =f"----- ganador {self.turno} -----"
-                return (True,self.estado)
-            if self.validador.validar_empate(self.tablero,self.turnos_jugados):
+            if self.validador.validar_ganador(self.tablero, fila, columna, self.turno):
+                self.estado = f"----- ganador {self.turno} -----"
+                return (True, self.estado)
+            if self.validador.validar_empate(self.tablero, self.turnos_jugados):
                 self.estado = "----- hay empate -----"
-                return (True,self.estado)
+                return (True, self.estado)
             if self.turno == "A":
                 self.turno = "B"
             else:
                 self.turno = "A"
-            return (True,"----- jugada valida -----")
+            return (True, "----- jugada valida -----")
         
         else:
-            return (False,"----- jugada invalida -----")
+            return (False, "----- jugada invalida -----")
         
--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from juego import Juego
 from tablero import Tablero
 
-def mostrar_tablero(tablero:Tablero):# muestra el tablero
-    print( "   "+" ".join ( f"{i+1:>2}"  for i in range(tablero.dimension))) #fila de indices
+def mostrar_tablero(tablero: Tablero):  # muestra el tablero
+    print( "   "+" ".join ( f"{i + 1 :>2}"  for i in range(tablero.dimension)))  #fila de indices
     for j in range(tablero.dimension):
-            print((f"{j+1:>2} "+" ".join(" ."if celda is None else f"{celda:>2}" for celda in tablero.grilla[j]))) #muestra el indice de columna + fila grilla
-def pedir_dimension():
+            print((f"{j + 1 :>2} "+" ".join(" ."if celda is None else f"{celda :>2}" for celda in tablero.grilla[j])))  #muestra el indice de columna + fila grilla
+def pedir_dimension():  # pide la dimension 
     while True:
         try:
             dimension = int(input("indica dimension del tablero(>=5): "))
-            if dimension <5:
+            if dimension < 5:
                 print("ERROR:dimension indicada muy pequeña, ingrese una dimension valida")
                 continue
             return dimension
                 
         except ValueError:
             print("ERROR:ingrese un numero entero por favor")
-def main():
+def main(): 
     dimension = pedir_dimension()
     juego = Juego(dimension)
     print("----- juego iniciado -----")
@@ -25,14 +25,14 @@ def main():
         print(f"----- Turno {juego.turno}----- ")
         fila = int(input("ingrese indice de la fila: ")) - 1
         columna = int(input("ingrese indice de la columna: ")) - 1
-        valida, mensaje =juego.jugada(fila,columna)
+        valida, mensaje = juego.jugada(fila, columna)
         print({mensaje})
     mostrar_tablero(juego.tablero)
     try:    
         seguir = int(input("\nvolver a jugar 1.si / 2.no : " ))
         if seguir == 1:
             main()
-        elif seguir ==2:
+        elif seguir == 2:
             print("----- gracias por jugar -----")
     except ValueError:
             print(" ingrese opcion valida (1 o 2)")
